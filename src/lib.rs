33e638b57585e47748427b62de28a7ee0851cee1
Viktor Popp <viktorpopp.dev@gmail.com>
2025-04-13 13:45:51 +0200

    Basics

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+enum Type {
+    Regular,
+    Directory,
+    BlockDevice,
+    CharDevice,
+    SymbolicLink,
+    Socket,
+}
+
+enum OpenMode {
+    Read,
+    Write,
+    Append,
+    Create,
+}
+
+struct MountManager {
+
+}
+
+trait Filesystem {
+    fn open(path: String, mode: OpenMode) -> Result<Vnode, String>;
+    fn read(node: Vnode) -> Result<Vec<u8>, String>;
+    fn close(node: Vnode) -> Result<(), String>;
+}
+
+struct Vnode {
+    pub name: String,
+    pub mode: OpenMode,
+    pub file_type: Type,
+    pub size: u64,
+    pub parent: Option<Box<Vnode>>,
+    pub children: Option<Vec<Vnode>>,
+}
