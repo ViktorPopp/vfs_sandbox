6f25eae2c202a40957baf7844bea1b4f8239a6b4
Viktor Popp <viktorpopp.dev@gmail.com>
2025-04-13 10:55:45 +0200

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug, Clone)]
+enum VnodeType {
+    Regular,
+    Directory,
+    BlockDevice,
+    CharDevice,
+    SymbolicLink,
+    Socket,
+}
+
+#[derive(Debug, Clone)]
+enum VfsError {
+    FileNotFound,
+    FileExists,
+    NotADirectory,
+    NotAFile,
+    MountPointNotFound,
+    MountPointInUse,
+    InvalidPath,
+    Other(String),
+}
+
 fn main() {
     println!("Hello, world!");
 }
