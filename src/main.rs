235dc968cd6c0b46c5daa4b9ab6e691915199bba
Viktor Popp <viktorpopp.dev@gmail.com>
2025-04-18 14:06:38 +0200

    Added _ to dummy fs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use vfs_sandbox::Vfs;
 use vfs_sandbox::fs::dummy::DummyFs;
 
 fn main() {
-    let dummy_vfs = Box::new(Vfs {
+    let _dummy_vfs = Box::new(Vfs {
         next: None,
         covers: None,
         ops: Some(Box::new(DummyFs)),
