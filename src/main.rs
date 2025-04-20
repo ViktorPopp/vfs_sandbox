7aecae7ae067b9f6b9a0e00880ca5e82c45430dd
Viktor Popp <viktorpopp.dev@gmail.com>
2025-04-20 10:53:55 +0200

    New tests

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ fn main() {
 
     // Mount them at different paths
     vfs.mount("/mem1", mem_fs1.clone());
-    vfs.mount("/mem2", mem_fs2.clone());
+    vfs.mount("/mem1/mem2", mem_fs2.clone());
 
     // Write to each filesystem
     vfs.write("/mem1/file.txt", "Hello from mem1");
-    vfs.write("/mem2/other.txt", "Greetings from mem2");
+    vfs.write("/mem1/mem2/other.txt", "Greetings from mem2");
 
     // Read from each filesystem
     if let Some(content) = vfs.read("/mem1/file.txt") {
         println!("mem1: {}", content);
     }
 
-    if let Some(content) = vfs.read("/mem2/other.txt") {
-        println!("mem2: {}", content);
+    if let Some(content) = vfs.read("/mem1/mem2/other.txt") {
+        println!("mem1/mem2: {}", content);
     }
 
     // Try reading a file not written
